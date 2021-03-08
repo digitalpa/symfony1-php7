e6d4eb4ce0fd0521e81ed89086a41d4667978322
cschirru <c.schirru@digitalpa.it>
2021-03-08 11:54:08 +0100

    Fix translations

--- lib/plugins/sfDoctrinePlugin/lib/record/sfDoctrineRecordI18nFilter.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/record/sfDoctrineRecordI18nFilter.class.php
@@ -51,7 +51,8 @@ class sfDoctrineRecordI18nFilter extends Doctrine_Record_Filter
   public function filterGet(Doctrine_Record $record, $name)
   {
     $culture = sfDoctrineRecord::getDefaultCulture();
-    if (isset($record['Translation'][$culture]) && '' != $record['Translation'][$culture][$name])
+    if ((isset($record['Translation'][$culture]) || $record['Translation'][$culture]) && '' != $record['Translation'][$culture][$name])
+//    if (isset($record['Translation'][$culture]) && '' != $record['Translation'][$culture][$name])
     {
       return $record['Translation'][$culture][$name];
     }
